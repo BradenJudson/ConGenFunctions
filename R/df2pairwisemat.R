75939fcf2b023579d435e70917bedbdb64c34217
BradenJudson <bradenjudson95@gmail.com>
2024-09-26 14:56:03 -0700

    Initial commit

---
+++ R/df2pairwisemat.R
@@ -0,0 +1,34 @@
+#' @title Dataframe to pairwise matrix converter
+#' @description
+#' Converts a long-form dataframe containing pairwise values into a symmetrical matrix.
+#'
+#' @param df A long-form dataframe containing two columns of grouping variables and a column of values.
+#' @param var Column name containing the value of interest (to populate the matrix).
+#' @param grp1 Grouping variable column 1 (e.g., population).
+#' @param grp2 Grouping variable column 2 (e.g., population).
+#'
+#' @return Distance matrix of the class Matrix.
+#' @import rlang
+#' @importFrom stats xtabs
+#' @importFrom stats as.formula
+#' @importFrom dplyr mutate
+#' @importFrom dplyr relocate
+#' @importFrom magrittr '%>%'
+#' @importFrom rlang .data
+#' @export
+
+df2pmat <- \(df, var, grp1, grp2) {
+
+  df_mat <- as.data.frame.matrix(stats::xtabs(as.formula(paste(var, "~", grp1, "+", grp2)), data = df))
+  mrow   <- colnames(df_mat)[!colnames(df_mat) %in% rownames(df_mat)]; print(mrow)
+  mcol   <- rownames(df_mat)[!rownames(df_mat) %in% colnames(df_mat)]; print(mcol)
+
+  adjmat <- rbind(df_mat %>% mutate({{mcol}} := 0) %>% relocate({{mcol}}, 1),
+                        as.data.frame(matrix(data = 0, nrow = 1, ncol = ncol(df_mat)+1,
+                                       dimnames = list(c(mrow), c(colnames(df_mat), mcol))), row.names = mrow))
+
+  adjmat[lower.tri(adjmat)] <- t(adjmat)[lower.tri(adjmat)]
+  diag(adjmat) <- NA
+  return(adjmat)
+}
+
